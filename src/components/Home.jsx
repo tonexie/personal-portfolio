1763e8add19ef3f4760d1d2ea27808f04944fd0f
tonexie <tone.xie@gmail.com>
2024-01-26 18:18:15 +1300

    add initial homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { Profiler } from "react";
+import profileImg from "../assets/portfolio/anon-image.png";
+import { MdKeyboardArrowRight } from "react-icons/md";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div
+      name="home"
+      className="w-full h-screen 
+      bg-gradient-to-b from-black via-black to-gray-800 text-white"
+    >
+      <div
+        className="max-w-screen-lg mx-auto 
+      flex flex-col items-center justify-items-center 
+      h-full px-4 md:flex-row"
+      >
+        <div className="flex flex-col justify-center h-full">
+          <h2 className="text-4xl sm:text-7xl font-bold text-white">
+            I'm a Full Stack Developer
+          </h2>
+          <p className="text-gray-500 py-4 max-w-md">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
 
-export default Home
+          <div>
+            <button
+              className="group text-white w-fit px-6 py-3 my-2
+            flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+            >
+              Portfolio
+              <span className="group-hover:rotate-90 duration-300">
+                <MdKeyboardArrowRight size={25} className="ml-1" />
+              </span>
+            </button>
+          </div>
+        </div>
+
+        <div>
+          <img
+            src={profileImg}
+            alt="My profile img"
+            className="rounded-2xl mx-auto w-2/3 md:w-full"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;

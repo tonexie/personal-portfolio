3ba37f3e20c47b34f6e399fb324430f8ea2b70d8
tonexie <tone.xie@gmail.com>
2024-01-26 17:22:49 +1300

    add initial navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Home from "./components/Home"
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Navbar />
+      <Home />
     </div>
   );
 }

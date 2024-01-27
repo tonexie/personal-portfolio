8693aafae2f1a58c8ec9e15a0c82938f3255a77f
tonexie <tone.xie@gmail.com>
2024-01-28 02:30:11 +1300

    fix mobile views

--- src/tailwind-utils.js
+++ src/tailwind-utils.js
@@ -1,3 +1,3 @@
 // Component parent styling
-export const bgGrayToBlack = 'w-full h-screen md:h-full md:py-40 px-2 sm:px-12 bg-gradient-to-b from-gray-800 to-black text-white';
-export const bgBlackToGray = 'w-full h-screen md:h-full md:py-40 px-2 sm:px-12 bg-gradient-to-b from-black to-gray-800 text-white';
+export const bgGrayToBlack = 'w-full h-full py-40 px-2 sm:px-12 bg-gradient-to-b from-gray-800 to-black text-white';
+export const bgBlackToGray = 'w-full h-full py-40 px-2 sm:px-12 bg-gradient-to-b from-black to-gray-800 text-white';
